000478dd46d6cd0bd881aa76e585f121b21f0695
CJ Lazell <cjlazell@gmail.com>
2023-08-16 20:13:02 -0500

    fix for find_open_port

--- weba/utils.py
+++ weba/utils.py
@@ -4,15 +4,14 @@ from .env import env
 
 
 def find_open_port(port: int = env.port, max_port: int = 65535):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     while port <= max_port:
-        location = (env.host, port)
-        check = sock.connect_ex(location)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            location = (env.host, port)
+            check = sock.connect_ex(location)
 
-        if check != 0:
-            return port
+            if check != 0:
+                return port
 
-        port += 1
+            port += 1
 
     raise IOError("no free ports")
